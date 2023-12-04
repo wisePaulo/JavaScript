bad08b6d10eedb1d8b462766c0b679a7e302cf42
wisePaulo <134930481+wisePaulo@users.noreply.github.com>
2023-12-04 17:37:21 +0000

    First commit

---
+++ arrays.js
@@ -0,0 +1,9 @@
+// Shopping list
+
+let supermarket = "Mercadona"
+let voucher = false
+let client = "Paulo"
+let shoppinglist = ["broccoli" , "bean" , "rice" , "meat" , "icecream" ]
+
+// Phrases 
+console.log(client +" goes to " + supermarket + " to buy " + shoppinglist)
---
+++ variaveis.js
@@ -0,0 +1,12 @@
+var nome = "Paulo";
+console.log(nome)
+
+let videogame = { 
+    brand: "Playstation 5",
+    year: 2022,
+    price: 549
+}
+
+ console.log(videogame)
+
+ console.log(" The " + videogame.brand + " is expensive ")
